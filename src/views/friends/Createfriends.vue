16d6ab910ca13f4a998584001b708aa798912b5c
wulanPratami21 <72369595+wulanPratami21@users.noreply.github.com>
2021-04-08 17:39:52 +0700

    tugas 4
    tugas  4

--- src/views/friends/Createfriends.vue
+++ src/views/friends/Createfriends.vue
@@ -2,18 +2,27 @@
 <div class="card shadow mt-3">
   <div class="card-body">
     <h5 class="card-title">Add Friends</h5>
-        <form class="row g-3">
+        <form class="row g-3" @submit.prevent="store">
       <div class="col-md-6">
         <label for="inputEmail4" class="form-label">Nama</label>
-        <input type="text" class="form-control" id="inputEmail4">
+        <input type="text" class="form-control" id="inputEmail4" v-model="friend.nama">
+        <div class="alert alert-danger" v-if="validation.nama">
+          {{ validation.nama[0] }}
+        </div>
       </div>
       <div class="col-md-6">
-        <label for="inputPassword4" class="form-label">No Tlp</label>
-        <input type="number" class="form-control" id="inputPassword4">
+        <label for="inputPassword4" class="form-label">No Telp</label>
+        <input type="number" class="form-control" id="inputPassword4" v-model="friend.no_telp">
+      <div class="alert alert-danger" v-if="validation.no_telp">
+          {{ validation.no_telp[0] }}
+        </div>
       </div>
       <div class="col-12">
         <label for="inputAddress" class="form-label">Alamat</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="Masukan Alamat">
+        <input type="text" class="form-control" id="inputAddress" placeholder="Masukan Alamat" v-model="friend.alamat">
+        <div class="alert alert-danger" v-if="validation.alamat">
+          {{ validation.alamat[0] }}
+        </div>
       </div>
       <div class="col-12">
         <button type="submit" class="btn btn-primary">Add</button>
@@ -22,3 +31,46 @@
   </div>
 </div>
 </template>
+<script>
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+export default {
+  setup() {
+    
+    const friend = reactive({
+      nama: '',
+      no_telp: '',
+      alamat: ''
+    })
+
+    const validation = ref([])
+
+    const router = useRouter()
+
+    function store(){
+      let nama = friend.nama
+      let no_telp = friend.no_telp
+      let alamat = friend.alamat
+
+      axios.post('http://127.0.0.1:800/api/friends', {
+        nama: nama,
+        no_telp: no_telp,
+        alamat: alamat
+      }).then(() => {
+        router.push({
+          name: 'Home'
+        })
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+    return {
+      friend,
+      validation,
+      router,
+      store
+    }
+  },
+}
+</script>
