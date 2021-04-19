723d752d7339bbc1c798ec3ac51fac791f9a9655
wulanPratami21 <72369595+wulanPratami21@users.noreply.github.com>
2021-04-19 21:08:10 +0700

    TUGAS 5
    TUGAS 5

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/friends/Home.vue'
 import Createfriends from '../views/friends/Createfriends.vue'
 import Editfriends from '../views/friends/Editfriends.vue'
-
+import Groups from '../views/groups/Index.vue'
 
 const routes = [
   {
@@ -25,6 +25,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: Editfriends
+  },
+  {
+    path: '/groups',
+    name: 'groups.index',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Groups
   }
 ]
 
