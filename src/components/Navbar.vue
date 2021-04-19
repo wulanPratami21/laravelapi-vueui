6cccdd53a00e25f2a7bfdc086325f89c099c1120
wulanPratami21 <72369595+wulanPratami21@users.noreply.github.com>
2021-04-19 21:07:31 +0700

    TUGAS 5
    TUGAS 5

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,30 @@
 <template>
 <div class="container">
-  <nav class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="#"> Contact App </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <router-link class="nav-link" to="/">Home  </router-link>  
-      </li>
-      <!-- <li class="nav-item">
-        <router-link class="nav-link" to="/createfriends"> Create friends </router-link> 
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/editfriends"> Edit friends </router-link>
-      </li> -->
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Contact App</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/groups">Groups</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
 </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
+
