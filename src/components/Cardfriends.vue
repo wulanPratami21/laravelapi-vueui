a01fcb62b1895ce6f01136771d9bd82a77dc3d1a
wulanPratami21 <72369595+wulanPratami21@users.noreply.github.com>
2021-06-30 17:25:21 +0700

    tugas 9
    tugas 9

---
+++ src/components/Cardfriends.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="row">
+    <div class="col-lg-4 mt-3" v-for="(friend, index) in friends" :key="index">
+<div class="card">
+  <div class="card-header">{{friend.nama}}</div>
+  <div class="card-body">
+    <h5 class="card-title">{{friend.no_telp}}</h5>
+    <p class="card-text">{{friend.alamat}}</p>
+  </div>
+  <div class="card-footer text-muted">
+    <router-link class="btn btn-primary" :to="{name:'Detailfriends', params:{id:friend.id}}">Detail</router-link>
+  </div>
+</div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    props: ["friends"],
+};
+</script>
+
