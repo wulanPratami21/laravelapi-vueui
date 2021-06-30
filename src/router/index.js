b65c40f3288aa1e8e87b93d13f3fe8aac1336cab
wulanPratami21 <72369595+wulanPratami21@users.noreply.github.com>
2021-06-30 17:24:52 +0700

    tugas 9
    tugas 9

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/friends/Home.vue'
 import Createfriends from '../views/friends/Createfriends.vue'
 import Editfriends from '../views/friends/Editfriends.vue'
+import Detailfriends from '../views/friends/Detailfriends.vue'
 import Groups from '../views/groups/Index.vue'
 
 const routes = [
@@ -25,6 +26,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: Editfriends
+  },
+  {
+    path: '/detailfriends/:id',
+    name: 'Detailfriends',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Detailfriends
   },
   {
     path: '/groups',
